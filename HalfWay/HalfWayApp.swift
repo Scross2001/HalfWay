b0dbcab43c687ad7ffb3b9f6f3e90eb62f1c8fb0
Samuel <Samuelcross2001@gmail.com>
2025-01-15 21:33:39 -0500

    Initial Commit

---
+++ HalfWay/HalfWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfWayApp.swift
+//  HalfWay
+//
+//  Created by Sam Cross on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HalfWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
